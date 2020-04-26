ebcfdd6bb7b4d9829e599f71730ab628baaee33e
annant05 <annantguptaneema@gmail.com>
2020-04-26 15:39:12 +0530

    added nat gateway

--- launchstack.sh
+++ launchstack.sh
@@ -1,1 +1,3 @@
-aws cloudformation create-stack --stack-name $1 --template-body  file://cfn.yaml  
+SNAME=$1
+aws cloudformation create-stack --stack-name ${SNAME} --template-body  file://cfn.yaml  
+echo "aws cloudformation delete-stack --stack-name ${SNAME}" > delete_stack.sh
